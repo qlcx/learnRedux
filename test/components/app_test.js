3e82f0f5383377ef8450751d9ef9d1ee280029cf
qlcx <1136425019@qq.com>
2016-09-03 22:33:59 +0800

    add comment-list&test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+/*
 // use 'describe' to group together similar tests 
 // 'App' 可以是任意字符串
 describe('App', () => {
@@ -13,5 +14,22 @@ describe('App', () => {
 
     // use 'expect' to make an 'assertion' about a target
     expect(component).to.contain('React simple starter');
+  });
+});
+*/
+
+describe('App', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
   });
 });
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,18 +2,37 @@ import { renderComponent, expect } from '../test_helper';
 import CommemtBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommemtBox);
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommemtBox);
     expect(component).to.have.class('comment-box');
-  })
+  });
 
   it('has a text area', () => {
-    const component = renderComponent(CommemtBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommemtBox);
     expect(component.find('button')).to.exist;
+  });
+
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
   });
 });

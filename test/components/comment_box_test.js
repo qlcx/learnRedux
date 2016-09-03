31415f022ad91cc77f3d71daa407b59bf0abb351
qlcx <1136425019@qq.com>
2016-09-03 11:00:41 +0800

    add comment-box

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,12 +2,18 @@ import { renderComponent, expect } from '../test_helper';
 import CommemtBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  it('has the correct class', () => {
+    const component = renderComponent(CommemtBox);
+    expect(component).to.have.class('comment-box');
+  })
+
   it('has a text area', () => {
     const component = renderComponent(CommemtBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-
+    const component = renderComponent(CommemtBox);
+    expect(component.find('button')).to.exist;
   });
 });

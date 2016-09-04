ace06fdeead7583d539fa923151bfc8f068e342c
qlcx <1136425019@qq.com>
2016-09-04 11:00:11 +0800

    add comment-list&test

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 const CommentList = (props) => {
   const list = props.comments.map(comment => <li key={comment}>{comment}</li>);
   
-  console.log(props.comments);
   return (
     <ul className="comment-list">
       {list}

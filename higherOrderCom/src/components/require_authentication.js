b9bd1a2b87313224d7d33fca704c93b0268d66ca
qlcx <1136425019@qq.com>
2016-09-06 22:10:30 +0800

    higherOrderComponent over

--- higherOrderCom/src/components/require_authentication.js
+++ higherOrderCom/src/components/require_authentication.js
@@ -12,6 +12,18 @@ export default function(ComposedComponent) {
       router: React.PropTypes.object,
     }
 
+    componentWillMount() {
+      if(!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedComponent {...this.props} />
     }

a2aaf978d7cedb7ebabb9f37311d13c11e378020
qlcx <1136425019@qq.com>
2016-09-06 20:32:06 +0800

    add a higher order component

---
+++ higherOrderCom/src/components/require_authentication.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  /*
+  Authentication.contextTypes = {
+    router: React.PropTypes.object,
+  }
+  */
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object,
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}

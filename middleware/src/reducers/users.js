5d5423faa790ae9fa518942e8b208455c1f0f11a
qlcx <1136425019@qq.com>
2016-09-07 20:30:16 +0800

    add ajax request

---
+++ middleware/src/reducers/users.js
@@ -0,0 +1,14 @@
+import {
+  FETCH_USERS
+} from '../actions/types';
+
+export default function( state=[], action) {
+  switch(action.type) {
+    case FETCH_USERS:
+      /* 断点测试 */
+      //debugger;
+      return [ ...state, ...action.payload ];
+  }
+
+  return state;
+}

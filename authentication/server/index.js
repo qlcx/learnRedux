e855e85e2d3068613e6eeb0231e70169453d76d1
qlcx <1136425019@qq.com>
2016-09-09 19:45:50 +0800

    add express & nodemon

--- authentication/server/index.js
+++ authentication/server/index.js
@@ -1,1 +1,17 @@
 // Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// App Setup
+app.use(morgan('combined')); // When incoming request, logging
+app.use(bodyParser.json({ type: '*/*' }));  // parser incoming request 
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port)

debd4f1473f1057cfcff30121d1d63e21ff97428
qlcx <1136425019@qq.com>
2016-09-12 20:42:27 +0800

    client add axios & post server

--- authentication/client/src/actions/index.js
+++ authentication/client/src/actions/index.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
 
+const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser( { email, password }) {
   return dispatch => {
     // Submit email/password to the server
-    
+    // { email, password } => { email: email, password: password }
+    axios.post(`${ROOT_URL}/signin`, { email, password });
+
     // If request is good..
     // - Update state to indicate user is authenticated
     // - Save the JWT token
--- authentication/client/src/components/auth/signin.js
+++ authentication/client/src/components/auth/signin.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
 
 
 class Signin extends Component {
 	handleFormSubmit({email, password}) {
 		console.log(email, password);
 		// Need to do something to log user in
+
+		this.props.signinUser({ email, password });
 	}
 	render() {
 		// `handleSubmit`是 `redux-form`提供的方法
@@ -30,4 +33,4 @@ class Signin extends Component {
 export default reduxForm({
 	form: 'signin',
 	fields: ['email', 'password']
-})(Signin);
+}, null, actions)(Signin);

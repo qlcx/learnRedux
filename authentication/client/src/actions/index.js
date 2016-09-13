a9e3d8866d0d167cd8008542e7757ca13b34c480
qlcx <1136425019@qq.com>
2016-09-13 13:46:37 +0800

    update auth state

--- authentication/client/src/actions/index.js
+++ authentication/client/src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, UNAUTH_USER } from './types'; 
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,14 +8,18 @@ export function signinUser( { email, password }) {
   return dispatch => {
     // Submit email/password to the server
     // { email, password } => { email: email, password: password }
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // If request is good..
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - redirect to the route '/feature'
-
-    // If request is bad...
-    // - Show an error to the user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good..
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER })
+        // - Save the JWT token
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+      })
   }
 }
--- authentication/client/src/reducers/index.js
+++ authentication/client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 //import { reducer } from 'redux-form';
+import authReducer from './auth_reducer';
 
 const rootReducer = combineReducers({
   form, // => form: form 
   //form: reducer
+  auth: authReducer,
 });
 
 export default rootReducer;
